71dedf9337c899f6bba689a8f0514ecb2ac9b986
Don Ettore <don.ettore@gmail.com>
2020-02-29 18:51:30 -0500

    more fun with dictionaries

---
+++ inventory.py
@@ -0,0 +1,20 @@
+#stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+
+def addToInventory(inventory, addedItems):
+    for item in addedItems:
+        inventory.setdefault(item, 0)
+        inventory[item] = inventory[item] + 1
+    return inventory
+
+def displayInventory(inventory):
+    print("Inventory:")
+    item_total = 0
+    for k, v in inventory.items():
+        print(str(v) + ' ' + k)
+        item_total = item_total + v
+    print("\nTotal number of items: " + str(item_total))
+
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = addToInventory(inv, dragonLoot)
+displayInventory(inv)
